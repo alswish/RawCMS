5ca53a4337e720ea6c56ebd63cf802d293f29ce2
Leonardo Altieri <leonardo.altieri-ext@prada.com>
2019-12-30 17:14:38 +0100

    DB Log entity added

---
+++ Plugins/RawCMS.Plugins.ApiGateway/Classes/Models/LogEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawCMS.Plugins.ApiGateway.Classes.Models
+{
+    public class LogEntity
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Ip { get; set; }
+        public string RequestHeaders { get; set; }
+        public string RequestContent { get; set; }
+        public string ResponseHeaders { get; set; }
+        public string ResponseContent { get; set; }
+        public int ResponseStatus { get; set; }
+        public DateTime RequestOn { get; set; }
+    }
+}
