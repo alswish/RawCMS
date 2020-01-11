f6bb66b766a1d34d7c0463bab62c0add309323cc
Leonardo Altieri <altieri.leon@gmail.com>
2020-01-11 16:42:40 +0100

    Stats controller added

---
+++ Plugins/RawCMS.Plugins.Core/Controllers/StatsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using RawCMS.Library.Core.Attributes;
+using RawCMS.Plugins.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawCMS.Plugins.Core.Controllers
+{
+    [AllowAnonymous]
+    [RawAuthentication]
+    [Route("api/[controller]")]
+    public class StatsController : Controller
+    {
+        [HttpGet("api-call")]
+        public RestMessage<ApiCallStats> Get()
+        {
+            return new RestMessage<ApiCallStats>(new ApiCallStats());
+        }
+    }
+}
---
+++ Plugins/RawCMS.Plugins.Core/Model/ApiCallStats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RawCMS.Plugins.Core.Model
+{
+    public class ApiCallStats
+    {
+        public int CurrentMonthTotal { get; set; }
+        public int Month1Total { get; set; }
+        public int Month2Total { get; set; }
+        public int Month3Total { get; set; }
+        public int Month4Total { get; set; }
+        public int Month5Total { get; set; }
+        public int Month6Total { get; set; }
+        public int Month7Total { get; set; }
+        public int Month8Total { get; set; }
+        public int Month9Total { get; set; }
+        public int Month10Total { get; set; }
+        public int Month11Total { get; set; }
+    }
+}
